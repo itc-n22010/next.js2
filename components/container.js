f170655c51205e22d9bc7628a21ce553d7de43e9
n22010 <n22010@std.it-college.ac.jp>
2023-02-15 06:08:09 +0000

    chap4.6

---
+++ components/container.js
@@ -0,0 +1,5 @@
+export default function Continer ({ children, large = false }) {
+  return (
+    <div>{children}</div>
+  )
+}
---
+++ components/post-body.js
@@ -0,0 +1,5 @@
+export default function PostBody ({ children }) {
+  return (
+    <div>{children}</div>
+  )
+}
