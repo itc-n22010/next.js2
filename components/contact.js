44ff4ec8693a34dc90b799bca3f5289fed9b886b
n22010 <n22010@std.it-college.ac.jp>
2023-02-15 06:12:53 +0000

    chap4.7

---
+++ components/contact.js
@@ -0,0 +1,8 @@
+export default function Contact () {
+  return (
+    <div>
+      <h3>Contact</h3>
+      <address>cube@web.mail.address</address>
+    </div>
+  )
+}
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
+import Contact from 'components/contact'
 
 export default function About () {
   return (
@@ -37,6 +38,7 @@ export default function About () {
           公開リポジトリを活用すると、全世界のどこからでもアクセスし、開発者が関連するプロジェクトのタスクを利用することができます。
         </p>
       </PostBody>
+      <Contact />
     </Container>
   )
 }
