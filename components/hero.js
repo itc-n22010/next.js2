30c3ddc942481950541b9fa866f0c6da7e3644ed
n22010 <n22010@std.it-college.ac.jp>
2023-02-15 06:49:27 +0000

    5.7

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,13 @@
+import Image from 'next/image'
+import cube from 'images/cube.jpg'
+
 export default function Hero ({ title, subtitle, imageOn = false }) {
   return (
     <div>
       <h1>{title}</h1>
       <p>{subtitle}</p>
-      {imageOn && <figure> [画像] </figure>}
+      {imageOn && (
+        <figure> <Image src={cube} alt='' layout='responsive' size='(min-width:1152px) 576px, (min-width:768px) 50vw, 100vw' priority placeholder='blur' /> </figure>)}
     </div>
   )
 }
