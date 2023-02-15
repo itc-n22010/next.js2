2df3eacc68e0367f02af7a16372e22e1e8dc2573
n22010 <n22010@std.it-college.ac.jp>
2023-02-15 06:58:11 +0000

    q

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import Layout from 'components/layout'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
 
 function MyApp ({ Component, pageProps }) {
   return (
