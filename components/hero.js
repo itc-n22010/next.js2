bb3ff2388526024dc1ee9083c569b2581d043096
n22010 <n22010@std.it-college.ac.jp>
2023-02-16 10:23:25 +0000

    6.3

--- components/hero.js
+++ components/hero.js
@@ -8,7 +8,7 @@ export default function Hero ({ title, subtitle, imageOn = false }) {
       <h1 className={styles.title}>{title}</h1>
       <p className={styles.subtitle}>{subtitle}</p>
       {imageOn && (
-        <figure> <Image src={cube} alt='' layout='responsive' size='(min-width:1152px) 576px, (min-width:768px) 50vw, 100vw' priority placeholder='blur' /> </figure>)}
+        <figure className={styles.image}> <Image src={cube} alt='' layout='responsive' sizes='(min-width:1152px) 576px, (min-width:768px) 50vw, 100vw' priority placeholder='blur' /> </figure>)}
     </div>
   )
 }
