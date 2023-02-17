0ef3572139833b8d74ce07c2afd07638fecb754b
n22010 <n22010@std.it-college.ac.jp>
2023-02-17 10:00:02 +0000

    9.5

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,10 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
 
-export default function Blog () {
+export default function Blog ({ posts }) {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
@@ -10,6 +12,16 @@ export default function Blog () {
         title='Blog'
         subtitle='Recent Possts'
       />
+      <Posts posts={posts} />
     </Container>
   )
+}
+export async function getStaticProps () {
+  const posts = await getAllPosts(4)
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
